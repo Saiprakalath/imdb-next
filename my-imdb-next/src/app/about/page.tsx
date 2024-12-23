be8084c9c969ee3f2200903675af763fd8b3d83f
Saiprakalath <prapusai50@gmail.com>
2024-12-23 16:26:14 +0530

    about  page added

---
+++ my-imdb-next/src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='max-w-6xl mx-auto p-3 space-y-4'><h1 className='text-2xl font-medium text-amber-600'>About</h1>
+            <p>Welcome to our movie database website! we are team of passionate movie enthusiasts who have come together to create a one-stop destination for all your movie-related needs.</p>
+            <p>InProgress...</p>
+            <p>InProgress...</p>
+        </div>
+    )
+}
+
+export default About
